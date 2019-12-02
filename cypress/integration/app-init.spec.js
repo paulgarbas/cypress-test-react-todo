e06fc72934b1f4da287c77d28afa7e48c346bc78
paulius.garbasauskas <paulius.garbasauskas@teltonika.lt>
2019-12-02 14:53:11 +0200

    end to end

---
+++ cypress/integration/app-init.spec.js
@@ -0,0 +1,27 @@
+describe('App initialization', () => {
+    it('loads todos on page load', () => {
+        cy.seedAndVisit()
+
+        cy.get('.todo-list li')
+            .should('have.length', 4)
+    })
+
+    it('displays an error on failure', () => {
+        cy.server()
+
+        cy.route({
+            url: 'api/todos',
+            method: 'GET',
+            status: 500,
+            response: {}
+        })
+
+        cy.visit('/')
+
+        cy.get('.todo-list li')
+            .should('not.exist')
+
+        cy.get('.error')
+            .should('be.visible')
+    })
+})
